99dba0049f013c32f9ecce3ae5ad9c665db34e62
Shrey Patel <shreynpatel23@gmail.com>
2021-02-11 09:24:18 +0530

    initial folder structure (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React from "react";
+import "./App.css";
+import CreateProjectModal from "./pop-ups/create-project/createProjectModal";
 
 function App() {
+  const [
+    toggleCreateProjectModal,
+    setToggleCreateProjectModal,
+  ] = React.useState(false);
   return (
-    <div>Hello from CleverX Assignment</div>
+    <div className="wrapper">
+      <div className="playground">
+        <button className="button" onClick={() => setToggleCreateProjectModal(true)}>
+          <p className="button_text">Create Project</p>
+        </button>
+      </div>
+      {toggleCreateProjectModal && <CreateProjectModal />}
+    </div>
   );
 }
 
---
+++ src/pop-ups/select-card/selectCardModal.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SelectCardModal() {
+    return (
+        <div>
+            hello from select card.
+        </div>
+    )
+}
+
+export default SelectCardModal
